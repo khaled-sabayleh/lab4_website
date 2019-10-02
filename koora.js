68b377dabe7bc80bde5fc3bac48d76f153aa2772
khaled sabayleh <engkhaledyassin96@gmail.com>
2019-10-03 01:23:33 +0300

    edit

--- koora.js
+++ koora.js
@@ -7,8 +7,19 @@ if(player === "17"){
     alert("sorry try again");
 }
 
-var player = prompt("who is the most team won in the world cup?");
-if(player === "brazil"){
+var player2 = prompt("who is the most team won in the world cup?");
+if(player2 === "brazil"){
+    alert("correct answer");
+}else{
+    alert("sorry try again");
+}
+
+
+var player3 = prompt("enter number equal 50 ?");
+while (player3 !=50){
+    player3 = prompt ("sory enter the correct number which is 50 ;");
+}
+if(player3 === "50"){
     alert("correct answer");
 }else{
     alert("sorry try again");
