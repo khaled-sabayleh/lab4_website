5d5d2fdd569d1efcd74d0c4a3db04c9aa9427e13
khaled sabayleh <engkhaledyassin96@gmail.com>
2019-10-03 03:14:40 +0300

    edit

--- koora.js
+++ koora.js
@@ -1,26 +1,8 @@
 
-
 var player = prompt("when the first humans played football (what is the ceantury)?");
+while (player != 17 ){
+ player = prompt("correct answer");
+}
 if(player === "17"){
-    alert("correct answer");
-}else{
-    alert("sorry try again");
-}
-
-var player2 = prompt("who is the most team won in the world cup?");
-if(player2 === "brazil"){
-    alert("correct answer");
-}else{
-    alert("sorry try again");
-}
-
-
-var player3 = prompt("enter number equal 50 ?");
-while (player3 !=50){
-    player3 = prompt ("sory enter the correct number which is 50 ;");
-}
-if(player3 === "50"){
-    alert("correct answer");
-}else{
-    alert("sorry try again");
+ alert("correct answer");
 }
