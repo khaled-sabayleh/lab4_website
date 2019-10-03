96615e17e7badada45dbe209e40322b7d030c3a0
khaled sabayleh <engkhaledyassin96@gmail.com>
2019-10-03 12:37:30 +0300

    edit

--- koora.js
+++ koora.js
@@ -1,9 +1,32 @@
 
-var player = prompt("when the first humans played football (what is the ceantury)");
-while (player != 17 ){
- player = prompt("insert 17");
-}
+var player = prompt("when the first humans played football (what is the ceantury)?");
 if(player === "17"){
- alert("correct answer");
+   alert("correct answer");
+}else{
+   alert("sorry try again");
 }
+var player = prompt("who is the most team won in the world cup?");
+if(player === "brazil"){
+   alert("correct answer");
+}else{
+   alert("sorry try again");
+}
+
+
+
+
+
+
+
+
+
+
+
+// var player = prompt("when the first humans played football (what is the ceantury)");
+// while (player != 17 ){
+//  player = prompt("insert 17");
+// }
+// if(player === "17"){
+//  alert("correct answer");
+// }
 
