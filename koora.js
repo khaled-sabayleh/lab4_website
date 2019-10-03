99ff525f84668b9f7c436df45431621d602727fc
khaled sabayleh <engkhaledyassin96@gmail.com>
2019-10-03 13:04:12 +0300

    edit

--- koora.js
+++ koora.js
@@ -15,11 +15,11 @@ if(player === "brazil"){
 
 
 
-// var player = prompt("when the first humans played football (what is the ceantury)");
-// while (player != 17 ){
-//  player = prompt("insert 17");
-// }
-// if(player === "17"){
-//  alert("correct answer");
-// }
+var player = prompt("when the first humans played football (what is the ceantury)");
+while (player != 17 ){
+ player = prompt("insert 17");
+}
+if(player === "17"){
+ alert("correct answer");
+}
 
