110b739806e7556ce45835e351dbeff78a9a387a
khaled sabayleh <engkhaledyassin96@gmail.com>
2019-10-03 10:28:10 +0300

    edit

--- koora.js
+++ koora.js
@@ -1,5 +1,5 @@
 
-var player = prompt("when the first humans played football (what is the ceantury)?");
+var player = prompt("when the first humans played football (what is the ceantury) (insert 17)");
 while (player != 17 ){
  player = prompt("correct answer");
 }
